9e69a14177110ec63e2e86a04e8dc7a22dd515c5
michele de bruyn <debruyn.mm@gmail.com>
2017-03-23 15:17:40 +0100

    Create beer controller, methods and routes

---
+++ app/controllers/beer_controller.rb
@@ -0,0 +1,61 @@
+class BeerController < ApplicationController
+  before_action :set_beer, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @beers = Beer.all
+  end
+
+  def show
+  end
+
+  def new
+    @beer = Beer.new
+  end
+
+  def create
+    @beer = Beer.new(beer_params)
+
+    if @beer.save
+      redirect_to beers_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @beer.update(beer_params)
+      redirect_to beers_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @beer.destroy
+    redirect_to beers_path
+  end
+
+  private
+
+  def set_beer
+    @beer = Beer.find(params[:id])
+  end
+
+  def beer_params
+    params.require(:beer).permit(
+      :title,
+      :description,
+      :country_of_origin,
+      :alcohol_percentage,
+      :brand,
+      :type,
+      :colour,
+      :classic,
+      :clarity,
+      :taste
+      )
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170323140637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "beers", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "country_of_origin"
+    t.float    "alcohol_percentage"
+    t.string   "brand"
+    t.string   "type"
+    t.string   "colour"
+    t.boolean  "classic"
+    t.integer  "clarity"
+    t.string   "taste"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
 
 end
