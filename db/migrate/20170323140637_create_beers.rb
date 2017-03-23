d184d9f2489328d8e05f03839e0e3ca5ad6c4b76
michele de bruyn <debruyn.mm@gmail.com>
2017-03-23 15:08:31 +0100

    First commit, initialize w/ beer model

---
+++ db/migrate/20170323140637_create_beers.rb
@@ -0,0 +1,18 @@
+class CreateBeers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :beers do |t|
+      t.string :title
+      t.text :description
+      t.string :country_of_origin
+      t.float :alcohol_percentage
+      t.string :brand
+      t.string :type
+      t.string :colour
+      t.boolean :classic
+      t.integer :clarity
+      t.string :taste
+
+      t.timestamps
+    end
+  end
+end
